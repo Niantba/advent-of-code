a08f9ed04b24ae18ca4b998261bacc345aa37d28
Audrey Baniant <audrey.baniant@gmail.com>
2024-12-05 12:57:41 -0500

    Solved 2b

--- 2024/2/2b.rb
+++ 2024/2/2b.rb
@@ -1,7 +1,6 @@
 require '../inputs/2.rb'
 
 input = INPUT.split("\n").map { |line| line.split.map(&:to_i) }
-safe =0
 
 def consecutive_diff?(line)
   line.each_cons(2).all? { |a, b| (a - b).abs == 1 || (a - b).abs == 2 || (a - b).abs == 3}
@@ -15,8 +14,21 @@ def decreasing?(line)
   line.each_cons(2).all? { |a, b| a > b }
 end
 
-input.each do |line|
-  consecutive_diff?(line) && (increasing?(line) || decreasing?(line)) ? safe += 1 : nil
+def is_safe?(line)
+  consecutive_diff?(line) && (increasing?(line) || decreasing?(line))
 end
+
+def safe_with_dampener?(line)
+  return true if is_safe?(line)
+
+  (0...line.length).each do |i|
+    modified_line = line[0...i] + line[i+1..-1]
+    is_safe?(modified_line)
+  end
+
+  false
+end
+
+safe = input.count { |line| safe_with_dampener?(line) }
 
 print safe
